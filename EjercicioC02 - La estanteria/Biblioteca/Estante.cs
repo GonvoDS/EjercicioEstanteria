57f3e0c90ac094a31a8f4819903c4a69ab5c8a0b
GonvoDS <gon.mat.d.s@gmail.com>
2022-09-05 18:55:55 -0300

    Ejercicio C02 La estanteria

---
+++ EjercicioC02 - La estanteria/Biblioteca/Estante.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Estante
+    {
+        private Producto[] productos;
+        private int ubicacionEstante;
+
+        private Estante(int capacidad)
+        {
+            this.productos = new Producto[capacidad];
+        }
+        public Estante(int ubicacion, int capacidad) : this(capacidad)
+        {
+
+            this.ubicacionEstante = ubicacion;
+        }
+        public Producto[] GetProducto()
+        {
+            return this.productos;
+        }
+        public static string MostrarEstante(Estante e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Ubicacion del estante {e.ubicacionEstante}");
+            foreach (Producto producto in e.productos)
+            {
+                sb.AppendLine(Producto.MostrarProducto(producto));
+            }
+            return sb.ToString();
+        }
+        public static bool operator ==(Estante e, Producto p)
+        {
+            foreach(Producto producto in e.productos)
+            {
+                if(producto==p)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool operator !=(Estante e, Producto p)
+        {
+            return !(e == p);
+        }
+
+        public static bool operator +(Estante e, Producto p)
+        {
+            if (e != p)
+            {
+                for (int i = 0; i < e.productos.Length; i++)
+                {
+                    if (e.productos[i] is null)
+                    {
+                        e.productos[i] = p;
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+        public static Estante operator -(Estante e, Producto p)
+        {
+            if(e==p)
+            {
+                for(int i = 0; i < e.productos.Length; i++)
+                {
+                    if(e.productos[i] == p)
+                    {
+                        e.productos[i] = null;
+                    }
+                }
+            }
+            return e;   
+        }
+    }
+}
---
+++ EjercicioC02 - La estanteria/Biblioteca/Producto.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    public class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+        public Producto(string marca, string codigoDeBarra, float precio)
+        {
+            this.marca = marca;
+            this.precio = precio;
+            this.codigoDeBarra = codigoDeBarra;
+        }
+        public string GetMarca()
+        {
+            return this.marca;
+
+        }
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+        public static string MostrarProducto(Producto p)
+        {
+            return $"Marca {p.marca} - Codigo {p.codigoDeBarra} - Precio {p.precio}";
+        }
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarra;
+        }
+        public static bool operator ==(Producto p, Producto p1)
+        {
+            if(p is not null && p1 is not null)
+            {
+                return p.codigoDeBarra == p1.codigoDeBarra && p.marca == p1.marca;
+            }
+            return false;
+        }
+        public static bool operator ==(Producto p,string marca)
+        {
+            return p.marca == marca;
+        }
+        public static bool operator !=(Producto p, Producto p1)
+        {
+            return !(p==p1);
+        }
+        public static bool operator !=(Producto p, string marca)
+        {
+            return !(p==marca);
+        }
+
+    }
+}
